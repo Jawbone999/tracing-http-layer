91824f95eba4fccb0e7d5758983025706b360a5f
Jonathan Henderson <jkhenderson999@gmail.com>
2022-08-05 12:54:09 -0500

    Export messenger

--- src/lib.rs
+++ src/lib.rs
@@ -7,5 +7,6 @@ mod messenger;
 pub use into_config::IntoHttpConfig;
 pub use layer::HttpLayer;
 pub use message::HttpConfig;
+pub use messenger::Messenger;
 
 pub use reqwest::{IntoUrl, Method, Url};
